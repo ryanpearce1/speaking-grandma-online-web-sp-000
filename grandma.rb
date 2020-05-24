637db7c43493173329edbe721746ef0ba13b7e9d
Ryan Pearce <ryan.pearce@key.me>
2020-05-24 15:31:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif 
+  elsif phrase.upcase == phrase
+    "NO, NOT SINCE 1938!"
+  else "HUH?! SPEAK UP, SONNY!"
 end
